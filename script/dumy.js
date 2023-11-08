59c7e9801798dabbb5a65e1cf60fff6d112fab92
haris <89023322+Haresz@users.noreply.github.com>
2023-11-08 09:22:26 +0700

    hasil kemarin, menambahkan data dumy struktur data dan merubah struktur file

---
+++ script/dumy.js
@@ -0,0 +1,30 @@
+[
+  {
+    id: "A1234",
+    judul: "IKIGAI",
+    penulis: "HECTOR GARCIA",
+    tahun: 2017,
+    isCompleted: none,
+  },
+  {
+    id: "A1235",
+    judul: "IKIGAI",
+    penulis: "HECTOR GARCIA",
+    tahun: 2017,
+    isCompleted: none,
+  },
+  {
+    id: "A1236",
+    judul: "IKIGAI",
+    penulis: "HECTOR GARCIA",
+    tahun: 2017,
+    isCompleted: none,
+  },
+  {
+    id: "A1237",
+    judul: "IKIGAI",
+    penulis: "HECTOR GARCIA",
+    tahun: 2017,
+    isCompleted: none,
+  },
+];
