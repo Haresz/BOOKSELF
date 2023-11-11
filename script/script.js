9c619887ac2cbd41c85b380a91c807f70d86a41e
haris <89023322+Haresz@users.noreply.github.com>
2023-11-11 08:46:48 +0700

    js module done

--- script/script.js
+++ script/script.js
@@ -1,48 +1,54 @@
-const rekomendasi = document.getElementById("coba");
+import { BOOKS } from "./dumy.js";
 
-// CARD
-const card = document.createElement("div");
-card.classList.add("card");
+for (let i = 0; i < BOOKS.length; i++) {
+  console.log(BOOKS[i].cover);
+  const rekomendasi = document.getElementById("coba");
 
-// CARD IMG
-const cardimg = document.createElement("img");
-cardimg.setAttribute("src", "./src/IKIGAI.jpg");
+  // CARD
+  const card = document.createElement("div");
+  card.classList.add("card");
 
-// P-JUDUL
-const judulbk = document.createElement("p");
-judulbk.classList.add("judul-buku");
-judulbk.textContent = "IKIGAI";
+  // CARD IMG
+  const cardimg = document.createElement("img");
+  cardimg.setAttribute("src", BOOKS[i].cover);
+  cardimg.setAttribute("alt", BOOKS[i].judul);
 
-// P-PENULIS
-const penulisbk = document.createElement("p");
-penulisbk.classList.add("penulis-buku");
-penulisbk.textContent = "HECTOR GARCIA";
+  // P-JUDUL
+  const judulbk = document.createElement("p");
+  judulbk.classList.add("judul-buku");
+  judulbk.textContent = BOOKS[i].judul;
 
-// HREF METHODS
-const hrefMethod = document.createElement("div");
-hrefMethod.classList.add("href-method");
-const edit = document.createElement("a");
-edit.setAttribute("href", "./edit.html");
-edit.textContent = "edit ||";
-const detail = document.createElement("a");
-detail.setAttribute("href", "./detail.html");
-detail.textContent = " detail";
+  // P-PENULIS
+  const penulisbk = document.createElement("p");
+  penulisbk.classList.add("penulis-buku");
+  penulisbk.textContent = BOOKS[i].penulis;
 
-// BUTTONS METHOD
-const containerBtn = document.createElement("div");
-containerBtn.classList.add("container-method-button");
-const buttonAdd = document.createElement("a");
-buttonAdd.classList.add("button-method", "button-add");
-buttonAdd.setAttribute("href", "./tambah.html");
-buttonAdd.textContent = "TAMBAH BUKU";
+  // HREF METHODS
+  const hrefMethod = document.createElement("div");
+  hrefMethod.classList.add("href-method");
+  const edit = document.createElement("a");
+  edit.setAttribute("href", "./edit.html");
+  edit.textContent = "edit ||";
+  const detail = document.createElement("a");
+  detail.setAttribute("href", "./detail.html");
+  detail.textContent = " detail";
 
-// MASUKIN
-rekomendasi.appendChild(card);
-card.appendChild(cardimg);
-card.appendChild(judulbk);
-card.appendChild(penulisbk);
-hrefMethod.appendChild(edit);
-hrefMethod.appendChild(detail);
-card.appendChild(hrefMethod);
-containerBtn.appendChild(buttonAdd);
-card.appendChild(containerBtn);
+  // BUTTONS METHOD
+  const containerBtn = document.createElement("div");
+  containerBtn.classList.add("container-method-button");
+  const buttonAdd = document.createElement("a");
+  buttonAdd.classList.add("button-method", "button-add");
+  buttonAdd.setAttribute("href", "./tambah.html");
+  buttonAdd.textContent = "TAMBAH BUKU";
+
+  // MASUKIN
+  rekomendasi.appendChild(card);
+  card.appendChild(cardimg);
+  card.appendChild(judulbk);
+  card.appendChild(penulisbk);
+  hrefMethod.appendChild(edit);
+  hrefMethod.appendChild(detail);
+  card.appendChild(hrefMethod);
+  containerBtn.appendChild(buttonAdd);
+  card.appendChild(containerBtn);
+}
