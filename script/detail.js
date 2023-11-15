c8dfd27446094aa5f5ecacdd9ae6bbe862f88453
haris <89023322+Haresz@users.noreply.github.com>
2023-11-15 08:38:10 +0700

    detail

--- script/detail.js
+++ script/detail.js
@@ -9,10 +9,70 @@ const coverBuku = document.getElementById("cover-detail");
 const judulBuku = document.getElementById("judul-detail");
 const pengarangBuku = document.getElementById("pengarang-detail");
 const deskripsiBuku = document.getElementById("deskripsi-detail");
+const containerBtn = document.getElementById("method-container-detail");
 
 // MENAMPILKAN DATA
-
 coverBuku.setAttribute("src", data.cover);
 judulBuku.innerHTML = data.judul;
 pengarangBuku.innerHTML = data.penulis;
 deskripsiBuku.innerHTML = data.deskripsi;
+
+// MENAMBAHKAN METHODS
+
+// ADD
+const buttonAdd = document.createElement("a");
+buttonAdd.setAttribute("id", "method-detail");
+buttonAdd.classList.add("button-method", "button-add");
+buttonAdd.addEventListener("click", (e) => {
+  alert("YAKIN INGIN NAMBAHIN INI ?");
+  location.reload(true);
+  data.isRecomended = false;
+  localStorage.setItem("data", JSON.stringify(convert));
+});
+buttonAdd.textContent = "TAMBAH BUKU";
+// DELETE
+const buttonDelete = document.createElement("a");
+buttonDelete.setAttribute("id", "method-detail");
+buttonDelete.classList.add("button-method", "button-delete");
+buttonDelete.addEventListener("click", (e) => {
+  alert("HAPUS BUKU ?");
+  location.reload(true);
+  data.splice(i, 1);
+  localStorage.setItem("data", JSON.stringify(convert));
+});
+buttonDelete.textContent = "DELETE";
+//REPLECE
+const buttonReplece = document.createElement("a");
+buttonReplece.setAttribute("id", "method-detail");
+buttonReplece.classList.add("button-method", "button-replece");
+buttonReplece.addEventListener("click", (e) => {
+  alert("REPLECE BUKU ?");
+  location.reload(true);
+  data.isCompleted = false;
+  localStorage.setItem("data", JSON.stringify(convert));
+});
+buttonReplece.textContent = "REPLECE";
+//DONE
+const buttonDONE = document.createElement("a");
+buttonDONE.setAttribute("id", "method-detail");
+buttonDONE.classList.add("button-method", "button-done");
+buttonDONE.addEventListener("click", (e) => {
+  alert("BUKU INI SUDAH SELESAI ?");
+  location.reload(true);
+  data.isCompleted = true;
+  localStorage.setItem("data", JSON.stringify(convert));
+});
+buttonDONE.textContent = "DONE";
+
+if (data.isRecomended == true) {
+  // MASUKIN
+  containerBtn.appendChild(buttonAdd);
+} else if (data.isRecomended == false && data.isCompleted == false) {
+  // MASUKIN
+  containerBtn.appendChild(buttonDelete);
+  containerBtn.appendChild(buttonDONE);
+} else if (data.isRecomended == false && data.isCompleted == true) {
+  // MASUKIN
+  containerBtn.appendChild(buttonDelete);
+  containerBtn.appendChild(buttonReplece);
+}
